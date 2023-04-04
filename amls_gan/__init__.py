a46c69ee8ac72e3d261b163580b0c2b55c87a08c
George Shaikovski <g.shaikov@gmail.com>
2023-04-04 10:21:19 +0100

    add random seed

--- amls_gan/__init__.py
+++ amls_gan/__init__.py
@@ -1,8 +1,14 @@
+import random
 from pathlib import Path
 
+import torch
 from environs import Env
 
 env = Env()
 
 RUN_DIR: Path = env.path("RUN_DIR")
 DATASETS_DIR: Path = env.path("DATASETS_DIR", default=Path.home() / "datasets")
+RND_SEED: int = env.int("RND_SEED", default=42)
+
+random.seed(RND_SEED)
+torch.manual_seed(RND_SEED)
